836c8dc39498e06bfb7e2b5cbd58f4c9d8152b25
Gabriel Garcia Crepaldi <12111bsi204@FACOM.LOCAL>
2022-10-03 20:09:44 -0300

    Resolucao

---
+++ LAB01 - Procedural/ex01/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!\n");
+        System.out.println("Nome: Gabriel Garcia Crepaldi\n");
+        System.out.println("Matricula: 12111BSI204\n");
+    }
+}
