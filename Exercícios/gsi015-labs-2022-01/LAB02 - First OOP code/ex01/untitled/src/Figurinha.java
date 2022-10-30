a00321437031b038e05bd3663152b9ae4b914168
Gabril <62163345+Gabril0@users.noreply.github.com>
2022-10-29 23:04:04 -0300

    Delete Exercícios/gsi015-labs-2022-01 directory

--- Exercícios/gsi015-labs-2022-01/LAB02 - First OOP code/ex01/untitled/src/Figurinha.java
+++
@@ -1,6 +0,0 @@
-import java.time.LocalDate;
-public class Figurinha {
-    String nomeDoJogador, pais, posicao;
-    LocalDate dataDeNascimento;
-    double altura, peso;
-}
--- Exercícios/gsi015-labs-2022-01/LAB02 - First OOP code/ex02/untitled/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Figurinha kevin = new Figurinha();
-        kevin.alteraNome("Kevin Shields");
-        kevin.alterarAltura(150.00);
-        kevin.alterarData("1950-12-12");
-    }
-}
