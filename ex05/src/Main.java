5c9d8334cd27db55ee83fd97647d10e74ab9fdf7
Gabril <62163345+Gabril0@users.noreply.github.com>
2022-10-31 20:25:11 -0300

    Add files via upload

---
+++ ex05/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Selecao sel = new Selecao();
+        sel.Selecao("Tecnico", "Pais");
+        sel.registraJogadores();
+    }
+}
