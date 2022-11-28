91f30b1f4a30e126967ea1028de83d675b3cbd3c
Gabril <62163345+Gabril0@users.noreply.github.com>
2022-11-28 15:20:58 -0300

    Delete 21-11 directory

--- 21-11/EXERCICIO/src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDate;
-
-public class Main {
-    public static void main(String[] args) {
-    FigurinhaExtra fig = new FigurinhaExtra("pais",2,5, LocalDate.parse("1415-12-12"),"nomeds","posicaoas","ouro","Legends");
-    System.out.printf("%s",fig.mostraFigurinha());
-    }
-}
